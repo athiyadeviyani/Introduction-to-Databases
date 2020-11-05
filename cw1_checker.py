84f60135d09039b031a16f636ebd25112ce19222
Athiya Deviyani <athiyadee@gmail.com>
2020-11-05 21:20:03 +0000

    Update checker script

--- cw1_checker.py
+++ cw1_checker.py
@@ -1,25 +1,43 @@
 forbidden = open("forbidden.txt", "r")
 forbidden_list = forbidden.readlines()
 forbidden_list = [x.replace('\n','') for x in forbidden_list]
+forbidden_list = [x.lower() for x in forbidden_list]
 # print(forbidden_list)
 
-cw1 = open("cw1.sql", "r")
-cw1_list = cw1.readlines()
-cw1_list = [x.replace('\n','') for x in cw1_list]
-cw1_list = [x.replace('(',' ') for x in cw1_list]
-cw1_list = [x.replace(')',' ') for x in cw1_list]
-cw1_list = [x.replace(';',' ') for x in cw1_list]
-tokens = []
-for line in cw1_list:
-    for word in line.split(' '):
-        if word != '':
-            tokens.append(word)
+forbidden_words = []
+
+filenames = ["01.sql",
+    "02.sql",
+    "03.sql",
+    "04.sql",
+    "05.sql",
+    "06.sql",
+    "07.sql",
+    "08.sql",
+    "09.sql",
+    "10.sql"]
+
+for fn in filenames: 
+    print('Checking ' + fn + '...')
+    filename = "submissions/" + fn
+    cw1 = open(filename, "r")
+    cw1_list = cw1.readlines()
+    cw1_list = [x.replace('\n','') for x in cw1_list]
+    cw1_list = [x.replace('(',' ') for x in cw1_list]
+    cw1_list = [x.replace(')',' ') for x in cw1_list]
+    cw1_list = [x.replace(';',' ') for x in cw1_list]
+    cw1_list = [x.lower() for x in cw1_list]
+    tokens = []
+    for line in cw1_list:
+        for word in line.split(' '):
+            if word != '':
+                tokens.append(word)
 # print(list(set(tokens)))
 
-forbidden_words = []
-for word in forbidden_list:
-    if word in tokens:
-        forbidden_words.append(word)
+
+    for word in forbidden_list:
+        if word in tokens:
+            forbidden_words.append(word)
 
 if len(forbidden_words) == 0:
     print('Congratulations! You have not used any forbidden SQL commands!')
